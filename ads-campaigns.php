5078a0704e37ca00d0ef79625e9853441a72858e
alitaj <alitaj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 17:18:51 +0000

    simplified plugin description
    git-svn-id: https://plugins.svn.wordpress.org/ads-campaigns/trunk@894445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ads-campaigns.php
+++ ads-campaigns.php
@@ -2,7 +2,7 @@
   /**
    * Plugin Name: Ads Campaigns
    * Plugin URI: http://corp.wishpond.com/
-   * Description: This plugin lets you quickly create great Ads on Google and Facebook directly from your Wordpress site!
+   * Description: Easily Create great Ads on Google, Facebook and Adroll!
    * Version: 1.0
    * Author: Wishpond
    * Text Domain: ads-campaigns
